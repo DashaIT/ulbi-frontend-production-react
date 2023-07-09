90ef0ab221048ed645cfacb4c981e7e4367f332b
Darya <filinova.darya00@mail.ru>
2023-07-10 06:59:36 +0900

    начали внедрять fsd, настроили абсолютные импорты

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import React, { Suspense, useContext, useState } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import './styles/index.scss';
-import { AboutPageAsync } from './pages/AboutPage/AboutePage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 export const App = () => {    
     const {theme, toggleTheme} = useTheme();
@@ -16,8 +16,8 @@ export const App = () => {
            
             <Suspense fallback={<div>Loading</div>}>
                 <Routes >
-                    <Route path={'/about'} element={<AboutPageAsync />}/>
-                    <Route path={'/'} element={<MainPageAsync />}/>
+                    <Route path={'/about'} element={<AboutPage />}/>
+                    <Route path={'/'} element={<MainPage />}/>
                 </Routes>
             </Suspense>                
            
