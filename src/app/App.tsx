90b353406f8558497757c091f430d9c8725a59bc
Darya <filinova.darya00@mail.ru>
2023-07-23 06:43:52 +0900

    12.Navbar.temps for dev. firts UI Kit component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,18 @@
 import React, { Suspense, useContext, useState } from 'react'
-import { Link, Route, Routes } from 'react-router-dom'
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme';
 import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widget/Navbar';
 
 
 export const App = () => {    
     const {theme, toggleTheme} = useTheme();
     return(
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
             <button onClick={toggleTheme}>Toggle</button>
-            <Link to='/'>Главная</Link>
-            <Link to='/about'>О нас</Link>
+            
             <AppRouter />     
         </div>
     )
