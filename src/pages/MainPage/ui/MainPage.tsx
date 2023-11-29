6dc6fa1c0852d31853fb86bfd6c700d681d2055c
DFilinova <you@example.com>
2023-11-29 22:22:33 +0300

    30. Redux-toolkit. Entity. Тесты на всех уровнях

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,12 @@
+import { Counter } from 'entities/Counter';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
     return (
         <div>
-            {t('Новый перевод')}
-            <br />
             {t('Главная страница')}
+            <Counter />
         </div>
     );
 };
