62897b29d13a3bc0a09e5241e59599052a97a6f5
Darya Filinova <darya.filinova@best2pay.net>
2024-04-15 03:41:42 +0300

    Статьи. EntityAdapter, thunks, slices. View selector

---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -0,0 +1,50 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { memo } from 'react';
+import ListIcon from 'shared/assets/icons/list.svg';
+import TiledIcon from 'shared/assets/icons/tiled.svg';
+import { Icon } from 'shared/ui/Icon/Icon';
+import { AppButton, ButtonTheme } from 'shared/ui/AppButton';
+import cls from './ArticleViewSelector.module.scss';
+import { ArticleView } from '../../model/types/article';
+
+interface ArticleViewSelectorProps {
+    className?: string;
+    view: ArticleView,
+    onViewClick?: (view: ArticleView) => void;
+}
+
+const viewTypes = [
+    {
+        view: ArticleView.SMALL,
+        icon: TiledIcon,
+    },
+    {
+        view: ArticleView.BIG,
+        icon: ListIcon,
+    },
+];
+
+export const ArticleViewSelector = memo((props: ArticleViewSelectorProps) => {
+    const { className, view, onViewClick } = props;
+
+    const onClick = (newView: ArticleView) => () => {
+        onViewClick?.(newView);
+    };
+
+    return (
+        <div className={classNames(cls.ArticleViewSelector, {}, [className])}>
+            {viewTypes.map((viewType, index) => (
+                <AppButton
+                    theme={ButtonTheme.CLEAR}
+                    onClick={onClick(viewType.view)}
+                    key={index}
+                >
+                    <Icon
+                        Svg={viewType.icon}
+                        className={classNames('', { [cls.notSelected]: viewType.view !== view })}
+                    />
+                </AppButton>
+            ))}
+        </div>
+    );
+});
