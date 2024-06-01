a84f580a83bb6f4345085f9acc3ff2553e167f5a
Darya Filinova <darya.filinova@best2pay.net>
2024-06-01 17:46:24 +0300

    62 CopyPlugin. Подготовка к продакшну. Публикуем на Netlify

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -4,20 +4,22 @@ export interface BuildPaths {
     entry: string;
     build: string;
     html: string;
-    src: string
+    src: string;
+    locales: string;
+    buildLocales: string;
+}
+
+export interface BuildEnv {
+    mode: BuildMode;
+    port: number;
+    apiUrl: string;
 }
 
 export interface BuildOptions {
     mode: BuildMode;
-    paths: BuildPaths,
-    isDev: Boolean,
-    port: number,
-    apiUrl: string,
-    project: 'storybook' | 'frontend' | 'jest'
-}
-
-export interface BuildEnv {
-    mode: BuildMode,
-    port: number,
-    apiUrl: string,
+    paths: BuildPaths;
+    isDev: boolean;
+    port: number;
+    apiUrl: string;
+    project: 'storybook' | 'frontend' | 'jest';
 }
