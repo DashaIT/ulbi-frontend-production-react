6348484ae57fafc728727b31af93d757cb358097
Darya Filinova <darya.filinova@best2pay.net>
2024-06-02 20:37:56 +0300

    74 Исправляем проблемы со STACK

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsComments/ArticleDetailsComments.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsComments/ArticleDetailsComments.tsx
@@ -35,7 +35,7 @@ export const ArticleDetailsComments = memo((props: ArticleDetailsCommentsProps) 
     });
 
     return (
-        <VStack gap="16" className={classNames('', {}, [className])}>
+        <VStack gap="16" max className={classNames('', {}, [className])}>
             <AppText
                 size={TextSize.L}
                 title={t('Комментарии')}
