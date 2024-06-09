37cb5671a20cb5195a418e4539d19f45bdf4217c
Darya Filinova <darya.filinova@best2pay.net>
2024-06-10 02:23:18 +0300

    102 Улучшаем и стандартизируем работу с роутером

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -12,8 +12,8 @@ import { AppButton, ButtonTheme } from '@/shared/ui/AppButton';
 import { AppText, AppTextTheme } from '@/shared/ui/AppText';
 import { HStack } from '@/shared/ui/Stack';
 import cls from './Navbar.module.scss';
-import { RoutePath } from '@/shared/const/router';
 import { AppLink, AppLinkTheme } from '@/shared/ui/AppLink';
+import { getRouteArticleCreate } from '@/shared/const/router';
 
 interface NavbarProps {
     className?: string;
@@ -41,7 +41,7 @@ export const Navbar = memo(({ className }: NavbarProps) => {
                     theme={AppTextTheme.INVERTED}
                 />
                 <AppLink
-                    to={RoutePath.article_create}
+                    to={getRouteArticleCreate()}
                     theme={AppLinkTheme.SECONDARY}
                     className={cls.createBtn}
                 >
