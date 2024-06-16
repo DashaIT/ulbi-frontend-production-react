b4a554fa34e99f77410f96a81d99c9d89579eb50
Darya Filinova <darya.filinova@best2pay.net>
2024-06-16 21:24:08 +0300

    125.2 концепция Feature flags. Постепенное внедрение новых фичей

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -1,22 +1,26 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
-import { User, UserSchema } from '../types/user';
+import { UserSchema, User } from '../types/user';
+import { setFeatureFlags } from '@/shared/lib/features';
 
 const initialState: UserSchema = {
     _inited: false,
 };
 
-const userSlice = createSlice({
+export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
         setAuthData: (state, action: PayloadAction<User>) => {
             state.authData = action.payload;
+            setFeatureFlags(action.payload.features);
         },
         initAuthData: (state) => {
             const user = localStorage.getItem(USER_LOCALSTORAGE_KEY);
             if (user) {
-                state.authData = JSON.parse(user);
+                const json = JSON.parse(user) as User;
+                state.authData = json;
+                setFeatureFlags(json.features);
             }
             state._inited = true;
         },
@@ -27,5 +31,6 @@ const userSlice = createSlice({
     },
 });
 
+// Action creators are generated for each case reducer function
 export const { actions: userActions } = userSlice;
 export const { reducer: userReducer } = userSlice;
