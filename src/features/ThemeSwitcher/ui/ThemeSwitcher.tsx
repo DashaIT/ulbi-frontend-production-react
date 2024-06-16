7e5b1856974110d8a4ecfe09ce8b02fd9558eba1
Darya Filinova <darya.filinova@best2pay.net>
2024-06-16 22:10:35 +0300

    127 Json Settings. Настройки пользователя. Localstorage на максималках

--- src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,12 +1,12 @@
-import { memo } from 'react';
+import React, { memo, useCallback } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
-
-import DarkIcon from '@/shared/assets/icons/theme-dark.svg';
 import LightIcon from '@/shared/assets/icons/theme-light.svg';
+import DarkIcon from '@/shared/assets/icons/theme-dark.svg';
 import { AppButton, ButtonTheme } from '@/shared/ui/AppButton';
-import cls from './ThemeSwitcher.module.scss';
-import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 import { Theme } from '@/shared/const/theme';
+import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
+import { saveJsonSettings } from '@/entities/User';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -14,12 +14,19 @@ interface ThemeSwitcherProps {
 
 export const ThemeSwitcher = memo(({ className }: ThemeSwitcherProps) => {
     const { theme, toggleTheme } = useTheme();
+    const dispatch = useAppDispatch();
+
+    const onToggleHandler = useCallback(() => {
+        toggleTheme((newTheme) => {
+            dispatch(saveJsonSettings({ theme: newTheme }));
+        });
+    }, [dispatch, toggleTheme]);
 
     return (
         <AppButton
             theme={ButtonTheme.CLEAR}
-            className={classNames(cls.themeswitcher, {}, [className])}
-            onClick={toggleTheme}
+            className={classNames('', {}, [className])}
+            onClick={onToggleHandler}
         >
             {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
         </AppButton>
