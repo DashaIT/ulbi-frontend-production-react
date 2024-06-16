294db263d247505e50d0266db6f669c57c15d196
Darya Filinova <darya.filinova@best2pay.net>
2024-06-16 22:21:10 +0300

    128 Запрос на получение пользователя. Избавляемся от заглушки в Localstorage

--- src/features/AuthByUserName/model/services/loginByUserName/loginByUserName.ts
+++ src/features/AuthByUserName/model/services/loginByUserName/loginByUserName.ts
@@ -1,7 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { ThunkConfig } from '@/app/providers/StoreProvider';
 import { User, userActions } from '@/entities/User';
-import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
+import { ThunkConfig } from '@/app/providers/StoreProvider';
 
 interface LoginByUsernameProps {
     username: string;
@@ -12,7 +11,7 @@ export const loginByUserName = createAsyncThunk<
     User,
     LoginByUsernameProps,
     ThunkConfig<string>
->('login/loginByUserName', async (authData, thunkApi) => {
+>('login/loginByUsername', async (authData, thunkApi) => {
     const { extra, dispatch, rejectWithValue } = thunkApi;
 
     try {
@@ -22,10 +21,6 @@ export const loginByUserName = createAsyncThunk<
             throw new Error();
         }
 
-        localStorage.setItem(
-            USER_LOCALSTORAGE_KEY,
-            JSON.stringify(response.data),
-        );
         dispatch(userActions.setAuthData(response.data));
         return response.data;
     } catch (e) {
